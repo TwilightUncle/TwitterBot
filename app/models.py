ac83af9cae82ded60e468db553113dc755502c78
水野 貴文 <mifukata-nozumi@outlook.jp>
2020-06-28 15:35:59 +0900

    add users table and relations

--- app/models.py
+++ app/models.py
@@ -1,19 +1,30 @@
 from app.database import db
+from datetime import datetime
 
 class TweetType(db.Model):
     __tablename__   = 'tweet_types'
     id              = db.Column(db.Integer, primary_key=True)
-    type_num        = db.Column(db.Integer)
-    interval        = db.Column(db.Integer)
-    name            = db.Column(db.Text)
-    create_at       = db.Column(db.DateTime)
-    update_at       = db.Column(db.DateTime)
+    type_num        = db.Column(db.Integer, nullable=False)
+    interval        = db.Column(db.Integer, nullable=False)
+    name            = db.Column(db.Text, nullable=False)
+    create_at       = db.Column(db.DateTime, default=datetime.now, nullable=False)
+    update_at       = db.Column(db.DateTime, default=datetime.now, nullable=False)
     tweets          = db.relationship("Tweet", backref="tweet_type")
 
 class Tweet(db.Model):
     __tablename__   = 'tweets'
     id              = db.Column(db.Integer, primary_key=True)
-    type_id         = db.Column(db.Integer, db.ForeignKey('tweet_types.id'))
-    text            = db.Column(db.Text)
-    create_at       = db.Column(db.DateTime)
-    update_at       = db.Column(db.DateTime)
+    type_id         = db.Column(db.Integer, db.ForeignKey('tweet_types.id'), nullable=False)
+    user_id         = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    text            = db.Column(db.Text, nullable=False)
+    create_at       = db.Column(db.DateTime, default=datetime.now, nullable=False)
+    update_at       = db.Column(db.DateTime, default=datetime.now, nullable=False)
+
+class User(db.Model):
+    __tablename__   = 'users'
+    id              = db.Column(db.Integer, primary_key=True)
+    name            = db.Column(db.Text, nullable=False)
+    password        = db.Column(db.Text, nullable=False)
+    create_at       = db.Column(db.DateTime, default=datetime.now, nullable=False)
+    update_at       = db.Column(db.DateTime, default=datetime.now, nullable=False)
+    tweets          = db.relationship("Tweet", backref="tweet_type")
