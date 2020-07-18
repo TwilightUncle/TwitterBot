81fa2c2e7b8736dbc61ef2244abcc2cc872a8be4
水野 貴文 <mifukata-nozumi@outlook.jp>
2020-07-18 18:38:04 +0900

    add common function

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,1 +1,10 @@
 # Pass path to parent directory
+
+def check_session_message(client, redirect_path:str, message:bytes):
+    '''リダイレクト直後のみmessageが表示されるか確認
+    '''
+    response = client.get(redirect_path)
+    assert message in response.data
+
+    response = client.get(redirect_path)
+    assert message not in response.data
