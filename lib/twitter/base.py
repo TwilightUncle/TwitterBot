6f74a4d0b77db534c81993fd16dfd660dc66bf88
水野 貴文 <mifukata-nozumi@outlook.jp>
2020-08-03 02:07:40 +0900

    add feature (validate method call correctness)

--- lib/twitter/base.py
+++ lib/twitter/base.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import abc
 import time
 import urllib.parse
@@ -11,7 +12,7 @@ import configparser
 import copy
 
 import random, string
-from lib.twitter.exception import TwitterRequiredParameterError
+from lib.twitter.exception import TwitterRequiredParameterError, TwitterAPIClientError
 
 
 def generateRandomString(n:int) -> str:
@@ -59,7 +60,11 @@ class TwitterApiBaseClient(object, metaclass=abc.ABCMeta):
         self.__request_params_raw = {}
 
         self.__endpoint         = self.__class__.__BASE_URL
-        self.__require_call_functions = self._getFunctionsCallRule()
+
+        self.__method_call_counter  = {}
+        self.__method_call_rules    = self.__getMethodCallRules()
+        self.__method_call_rules.update(self._getFunctionsCallRule())
+        self.__is_executed          = False
 
         # response
         self.__status           = None
@@ -67,13 +72,22 @@ class TwitterApiBaseClient(object, metaclass=abc.ABCMeta):
         self.__contents         = None
     
 
+    # -----------------------------------------------------------------------
+    # public methods
+    # -----------------------------------------------------------------------
+
+
     def setParam(self, key:str, value:str):
+        self._validateMethodCallCorrectness(sys._getframe().f_code.co_name)
         self.__request_params_raw[key] = value
 
 
     def exec(self) -> str:
         '''リクエスト実行
         '''
+        self._validateMethodCallCorrectness(sys._getframe().f_code.co_name)
+        self.__validateMethodCallCorrectness()
+
         query_param_string  = ''
         if len(self.__getRequestParams()) > 0:
             query_param_string  = '?' + urllib.parse.urlencode(self.__getRequestParams())
@@ -88,17 +102,30 @@ class TwitterApiBaseClient(object, metaclass=abc.ABCMeta):
             response        = res.read().decode('utf-8')
             self.__contents = json.loads(response)
         
+        self.__is_executed = True
+        
 
     def getStatus(self):
+        '''exec実行後呼び出し(結果)'''
+        self._validateMethodCallCorrectness(sys._getframe().f_code.co_name)
         return self.__status
     
 
     def getHeaders(self):
+        '''exec実行後呼び出し(結果)'''
+        self._validateMethodCallCorrectness(sys._getframe().f_code.co_name)
         return self.__headers
     
 
     def getContents(self):
+        '''exec実行後呼び出し(結果)'''
+        self._validateMethodCallCorrectness(sys._getframe().f_code.co_name)
         return self.__contents
+    
+
+    # -------------------------------------------------------------------------
+    # private methods
+    # -------------------------------------------------------------------------
 
 
     def __buildOAuthHeader(self) -> str:
@@ -154,6 +181,51 @@ class TwitterApiBaseClient(object, metaclass=abc.ABCMeta):
         return self.__request_params
     
 
+    def __getMethodCallRules(self) -> dict:
+        return {
+            'setParam' : {
+                'callable' : 'before_exec'
+            },
+            'exec' : {
+                'callable' : 'before_exec'
+            }
+            'getStatus' : {
+                'callable' : 'after_exec'
+            },
+            'getHeaders' : {
+                'callable' : 'after_exec'
+            },
+            'getContents' : {
+                'callable' : 'after_exec'
+            }
+        }
+
+    
+    def __validateMethodCallCorrectness(self):
+        '''execの最初に呼び出すことのみを想定。関数呼び出しの正当性の検証
+        '''
+        for k, v in self.__method_call_rules.items():
+            call_count  = self.__method_call_counter.get(k)
+
+            # required check
+            required    = v.get('required')
+            if required == True and (call_count is None or call_count <  1):
+                raise TwitterAPIClientError('"{}" must be called before "exec" call'.format(k))
+
+            # validation number of calls
+            count_rule  = b.get('call_count')
+            if count_rule:
+                min_count = count_rule.get('min')
+                if min_count and (call_count is None or call_count < min_count):
+                    raise TwitterAPIClientError('"{}" must be called at least {} times'.format(k, min_count))
+
+    
+
+    # -------------------------------------------------------------------------
+    # protedted methods
+    # -------------------------------------------------------------------------
+    
+
     def _addPath(self, path:str):
         '''前後のスラッシュは削除, 後ろに再度追加
         '''
@@ -164,6 +236,49 @@ class TwitterApiBaseClient(object, metaclass=abc.ABCMeta):
         '''エンドポイントの拡張子を指定
         '''
         self.__endpoint += '.' + ext
+    
+
+    def _validateMethodCallCorrectness(self, method_name:str):
+        '''呼び出しに条件があるメソッドはこれを呼びだす(検証対象の関数呼び出し時点で分かるものは呼び出しの段階で例外)
+        '''
+        # count number of call
+        if self.__method_call_counter.get(method_name) is None:
+            self.__method_call_counter[method_name] = 0
+        
+        self.__method_call_counter[method_name] += 1
+
+       # get rule 
+        rule = self.__method_call_rules.get(method_name)
+        if rule is None:
+            return
+        
+        # validation callable
+        callable_rule = rule.get('callable')
+        if callable_rule:
+            if callable_rule == 'before_exec' and self.__is_executed == True:
+                raise TwitterAPIClientError('"{}" cannot be called after executing exec.'.format(method_name))
+            if callable_rule == 'after_exec' and self.__is_executed == False:
+                raise TwitterAPIClientError('"{}" cannot be called before executing exec.'.format(method_name))
+        
+        # validation number of calls
+        count_rule = rule.get('call_count')
+        if count_rule:
+            if self.__is_executed == True:
+                raise TwitterAPIClientError('"{}" cannot be called after executing exec.'.format(method_name))
+            max_count = count_rule.get('max')
+            if max_count and max_count < self.__method_call_counter[method_name]
+                raise TwitterAPIClientError('"{}" can be called up to {} times.'.format(method_name, max_count))
+        
+        # required check
+        required = rule.get('required')
+        if required == True:
+            if self.__is_executed == True:
+                raise TwitterAPIClientError('"{}" cannot be called after executing exec.'.format(method_name))
+
+    
+    # -------------------------------------------------------------------------
+    # abstract methods
+    # -------------------------------------------------------------------------
 
 
     @abc.abstractmethod
