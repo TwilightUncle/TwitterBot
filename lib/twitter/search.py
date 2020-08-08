90b78b8c762cf9c1dfcc12d748d4b9a6685bc35e
水野 貴文 <mifukata-nozumi@outlook.jp>
2020-08-09 01:00:15 +0900

    add input parameters

--- lib/twitter/search.py
+++ lib/twitter/search.py
@@ -1,4 +1,6 @@
 import sys
+import datetime
+import calendar
 from lib.twitter.base import TwitterApiBaseClient, TwitterApiBaseInput, TwitterApiBaseOutput
 from lib.twitter.exception import TwitterAPIInputError
 
@@ -11,9 +13,13 @@ class TwitterApiSearchInput(TwitterApiBaseInput):
         super().__init__()
         self.__search_query = None
         self.__count        = None
+        self.__until        = None
+        self.__result_type  = None
     
 
     def setSearchQuery(self, query:str):
+        '''ツイートの検索文字列
+        '''
         MAX_LEN = 500
         if len(tweet) > MAX_LENGTH:
             raise TwitterAPIInputError('Too many characters. Excess:{}'.format(len(tweet) - MAX_LENGTH))
@@ -21,13 +27,44 @@ class TwitterApiSearchInput(TwitterApiBaseInput):
     
 
     def setCount(self, num:int):
+        '''取得するツイートの個数。
+        \n 指定できる範囲は 1 ～ 100
+        '''
         MAX_COUNT = 100
         MIN_COUNT = 1
         if num > MAX_COUNT:
-            TwitterAPIInputError('Input value is too large. max: {}'.format(MAX_COUNT))
+            raise TwitterAPIInputError('Input value is too large. max: {}'.format(MAX_COUNT))
         if num < MIN_COUNT:
-            TwitterAPIInputError('Input value is too small. max: {}'.format(MIN_COUNT))
+            raise TwitterAPIInputError('Input value is too small. max: {}'.format(MIN_COUNT))
         self.__count = str(num)
+    
+
+    def setUntil(self, year:int, month:int, day:int):
+        '''指定した年月日以前のツイートを取得する。遡れない場合もあるらしい
+        '''
+        current_date = datetime.date.today()
+        if year < 1 or year > current_date.year:
+            raise TwitterAPIInputError('invaild input year.')
+        if month < 1 or month > 12 or (year == current_date.year and month > current_date.month):
+            raise TwitterAPIInputError('invaild input month.')
+        if (
+            day < 1 or day > calendar.monthrange(year, month) or 
+            (year == current_date.year and month == current_date.month and day > current_date.day)
+        ):
+            raise TwitterAPIInputError('invaild input day')
+        self.__until = '{:0=4}-{:0=2}-{:0=2}'.format(year, month, day)
+    
+
+    def setResultType(self, result_type:str):
+        '''以下のいずれかを指定
+        \n mixed    ... 混合
+        \n recent   ... 最近のツイートのみ取得
+        \n popular  ... 人気のあるツイートのみ取得
+        '''
+        types = ['mixed', 'recent', 'popular']
+        if result_type not in types:
+            raise TwitterAPIInputError('invaild input param')
+        self.__result_type = result_type
     
 
     def _checkInputCorrectness(self):
@@ -37,6 +74,12 @@ class TwitterApiSearchInput(TwitterApiBaseInput):
 
         if self.__count is not None:
             super()._setQueryParam('count', self.__count)
+        
+        if self.__until is not None:
+            super()._setQueryParam('until', self.__until)
+        
+        if self.__result_type is not None:
+            super()._setQueryParam('result_type', self.__result_type)
 
 
 class TwitterApiSearchOutput(TwitterApiBaseOutput):
