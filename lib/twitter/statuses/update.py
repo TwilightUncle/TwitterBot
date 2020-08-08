f9f8a3f63156c1d01522c1ab0976398a9200ef1b
水野 貴文 <mifukata-nozumi@outlook.jp>
2020-08-08 22:21:31 +0900

    change way of input and output

--- lib/twitter/statuses/update.py
+++ lib/twitter/statuses/update.py
@@ -1,6 +1,57 @@
+import re
 import sys
-from lib.twitter.base import TwitterApiBaseClient
-from lib.twitter.exception import TwitterValidateParamaterError
+from lib.twitter.base import TwitterApiBaseClient, TwitterApiBaseInput, TwitterApiBaseOutput
+from lib.twitter.exception import TwitterValidateParamaterError, TwitterAPIInputError
+
+
+class TwitterApiStatusesUpdateInput(TwitterApiBaseInput):
+    '''tweet投稿入力'''
+
+
+    def __init__(self):
+        super().__init__()
+        self.__tweet            = None
+        self.__reply_target_id  = None
+        self.__is_sensitive     = None
+    
+
+    def setTweet(self, tweet:str):
+        '''投稿するtweet文
+        '''
+        MAX_LENGTH = 140
+        if len(tweet) > MAX_LENGTH:
+            raise TwitterAPIInputError('Too many characters. Excess:{}'.format(len(tweet) - MAX_LENGTH))
+        self.__tweet = tweet
+    
+
+    def setReplyTargetId(self, id:str):
+        '''リプする場合の対象tweet idをセット
+        '''
+        p = re.compile('\d+')
+        if p.fullmatch(id) is None:
+            raise TwitterAPIInputError('Only numbers can be specified in the argument.')
+        self.__reply_target_id = id
+    
+
+    def setIsSensitive(self, is_sensitive:bool):
+        self.__is_sensitive = 'true' if is_sensitive else 'false'
+    
+
+    def _checkInputCorrectness(self):
+        if self.__tweet is None:
+            raise TwitterAPIInputError('rquired parameter is not input.')
+        super()._setQueryParam('status', self.__tweet)
+
+        if self.__reply_target_id is not None:
+            super()._setQueryParam('in_reply_to_status_id', self.__reply_target_id)
+        
+        if self.__is_sensitive is None:
+            super()._setQueryParam('possibly_sensitive', self.__is_sensitive)
+
+
+class TwitterApiStatusesUpdateOutput(TwitterApiBaseOutput):
+    def __init__(self, results):
+        super().__init__(results)
 
 
 class TwitterApiStatusesUpdateClient(TwitterApiBaseClient):
@@ -13,56 +64,17 @@ class TwitterApiStatusesUpdateClient(TwitterApiBaseClient):
         super().__init__(api_key, api_secret, access_token, access_secret)
         super()._addPath('statuses/update')
         super()._addExtension('json')
-
-
-    def setTweet(self, tweet:str):
-        '''投稿するツイート文
-        '''
-        super()._validateMethodCallCorrectness(sys._getframe().f_code.co_name)
-
-        MAX_LENGTH = 140
-        if len(tweet) > MAX_LENGTH:
-            raise TwitterValidateParamaterError('Too many characters. Excess:{}'.format(len(tweet) - MAX_LENGTH))
-
-        super().setParam('status', tweet)
-    
-
-    def setReplyTargetId(self, id:str):
-        '''リプする場合の対象tweet idをセット
-        '''
-        super()._validateMethodCallCorrectness(sys._getframe().f_code.co_name)
-        super().setParam('in_reply_to_status_id', id)
-    
-
-    def setSensitive(self, is_sensitive:bool):
-        '''センシティブなツイートをするときtrue
-        '''
-        super()._validateMethodCallCorrectness(sys._getframe().f_code.co_name)
-        super().setParam('possibly_sensitive', 'true' if is_sensitive else 'false')
     
 
     # --------------------------
     # override functions
     # --------------------------
+
+
+    def exec(self, inp) -> TwitterApiStatusesUpdateOutput:
+        results = super().exec(inp)
+        return TwitterApiStatusesUpdateOutput(results)
     
 
     def _requestMethod(self) -> str:
         return 'POST'
-
-    
-    def _getRequiredParameterKeys(self) -> list:
-        return ['status']
-    
-
-    def _getFunctionsCallRule(self) -> dict:
-        return {
-            'setTweet' : {
-                'required' : True
-            },
-            'setReplyTargetId' : {
-                'callable' : 'before_exec'
-            },
-            'setSensitive' : {
-                'callable' : 'before_exec'
-            }
-        }
