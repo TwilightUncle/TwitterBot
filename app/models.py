ceda4218cb806c328dddc2b755d5ae87913ffc43
水野 貴文 <mifukata-nozumi@outlook.jp>
2020-08-11 19:39:33 +0900

    change model

--- app/models.py
+++ app/models.py
@@ -28,14 +28,15 @@ class User(db.Model):
     name            = db.Column(db.Text, nullable=False)
     password        = db.Column(db.Text, nullable=False)
     permission      = db.Column(db.Integer, default=constant.PERMISSION_NOMAL, nullable=False)
+    twitter_user_id = db.Column(db.Text)
     create_at       = db.Column(db.DateTime, default=datetime.now, nullable=False)
     update_at       = db.Column(db.DateTime, default=datetime.now, nullable=False)
     tweets          = db.relationship("Tweet", backref="tweets")
 
 
     @classmethod
-    def create(cls, name, password, permission=3):
-        user = cls(name=name, password=generate_password_hash(password), permission=permission)
+    def create(cls, name, password, permission=3, twitter_user_id=None):
+        user = cls(name=name, password=generate_password_hash(password), permission=permission, twitter_user_id=twitter_user_id)
         db.session.add(user)
         return user
     
@@ -45,10 +46,12 @@ class User(db.Model):
         return db.session.query(cls).all()
     
 
-    def update(self, name=None, password=None, permission=None):
+    def update(self, name=None, password=None, permission=None, twitter_user_id=None):
         if name:
             self.name = name
         if password:
             self.password = generate_password_hash(password)
         if permission:
             self.permission = permission
+        if twitter_user_id:
+            self.twitter_user_id = twitter_user_id
