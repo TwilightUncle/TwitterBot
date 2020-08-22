c198f54d416f74a53a5d960031be02230e339608
水野 貴文 <mifukata-nozumi@outlook.jp>
2020-08-22 15:57:24 +0900

    updated demo

--- lib/twitter/demo/profileImageUpload.py
+++ lib/twitter/demo/profileImageUpload.py
@@ -1,18 +1,11 @@
-from lib.twitter.account.update_profile_image import TwitterApiAccountUpdateProfileImageInput, TwitterApiAccountUpdateProfileImageClient
-from lib.twitter.media.upload import TwitterApiMediaUploadInput, TwitterApiMediaUploadClient
+from lib.twitter.account.update_profile_image import updateProfileImage
 
 
 if __name__ == '__main__':
     # 動作確認
     image_path = './app/static/image/icon1.png'
 
-    inp         = TwitterApiAccountUpdateProfileImageInput()
-    inp.setImageFilePath(image_path)
-    inp.setIncludeEntities(False)
-    inp.setSkipStatus(True)
-
-    client      = TwitterApiAccountUpdateProfileImageClient()
-    response    = client.exec(inp)
+    response    = updateProfileImage(image_path, False, True)
 
     print(response.getStatus())
     print(response.getUser())
