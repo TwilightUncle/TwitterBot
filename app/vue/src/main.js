bff2986fb9e6d11a869f884f07beee9333fdee21
mizuno <mifukata-nozumi@outlook.jp>
2020-08-30 20:39:03 +0900

    add router

--- app/vue/src/main.js
+++ app/vue/src/main.js
@@ -1,9 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+// import router from './router'
 
+// Vue.use(Router)
 Vue.config.productionTip = false
 
 new Vue({
+    // router,
     render: h => h(App),
     delimiters: ["[[", "]]"],   // 変数展開用の区切り文字を変更
 }).$mount('#app')
